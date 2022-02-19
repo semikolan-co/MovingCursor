dd4df7cd383de41a96bd54cd51538107f9363e07
Harsh Vishwakarma <64343449+meinhoonharsh@users.noreply.github.com>
2022-02-19 14:38:58 +0530

    Updated Index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,10 @@
 import * as React from 'react'
-import styles from './styles.module.css'
 import MovingCursor from './MovingCursor.js'
 
-interface Props {
-  text: string
-}
-
-export const ExampleComponent = ({ text }: Props) => {
+export const ExampleComponent = () => {
   return (
-    <div className={styles.test}>
+    <>
       <MovingCursor />
-      TypeScript: {text}
-    </div>
+    </>
   )
 }
