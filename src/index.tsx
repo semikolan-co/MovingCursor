bcefe0271d786cf064bd7d7996ef352fb047577c
Harsh Vishwakarma <64343449+meinhoonharsh@users.noreply.github.com>
2022-02-19 15:10:34 +0530

    Shifted into Default Export

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,2 @@
-import * as React from 'react'
 import MovingCursor from './MovingCursor.js'
-
-export const ExampleComponent = () => {
-  return (
-    <>
-      <MovingCursor />
-    </>
-  )
-}
+export default MovingCursor
